08f0aabe9c59fe05bd99477bd4632644f7b3e4b7
guitongZhang <zhangguitong@didiglobal.com>
2021-03-14 00:46:12 +0800

    chore: completed dependency config

--- logger/log4j/src/main/java/com/demo/test/logger/Main.java
+++ logger/log4j/src/main/java/com/demo/test/logger/Main.java
@@ -1,13 +1,16 @@
 package com.demo.test.logger;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author guitongZhang
  * @date 2021/03/14 00:14
  */
+@Slf4j
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(123);
+        log.info("123");
     }
 
 }
---
+++ logger/log4j/src/test/java/com/demo/test/logger/MainTest.java
@@ -0,0 +1,16 @@
+package com.demo.test.logger;
+
+import org.junit.Test;
+
+/**
+ * @author guitongZhang
+ * @date 2021/03/14 00:33
+ */
+public class MainTest {
+
+    @Test
+    public void test() {
+        System.out.println(123);
+    }
+
+}
